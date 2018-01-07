88f117e66a671a9325546f953bd802c7555758b7
unknown <jovanbelic234@gmail.com>
2018-01-07 23:42:00 +0100

    m

---
+++ views/controllers/app-controller.js
@@ -0,0 +1,19 @@
+let user_pic,
+	user_getImage;
+
+let user_name,
+	u_name;
+
+let job_title,
+	j_title;
+
+	user_pic = document.getElementById('user-image');
+	user_getImage = 'url(' + localStorage['profile_image'] + ')';
+
+	user_pic.style.backgroundImage = user_getImage;
+
+	user_name = localStorage['username'];
+	u_name = document.getElementById('u-name').innerHTML = user_name;
+
+	job_title = localStorage['jobTitle'];
+	j_title = document.getElementById('j-title').innerHTML = job_title;	
